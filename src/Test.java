cba9c75ef4bf2df6db844828914f735bf4c6dd61
BabyShung <hao-zheng@uiowa.edu>
2014-03-23 22:32:51 -0500

    ud

--- src/Test.java
+++ src/Test.java
@@ -10,12 +10,28 @@ public class Test {
 		VigenerCipher vc = new VigenerCipher(true);// true means having spaces
 		Statistics s = new Statistics();
 
-		String ct = op.readFile("vigenere_nosp.txt");// read from plaintext1.txt
-		System.out.println(ct);
-		s.alphabetStatistics(ct);
-		
-		s.frequency(ct, 0, 5);
+		String ct = op.readFile("vigenere_nosp.txt");// read from
+														// vigenere_nosp.txt
+		// String dt = op.readFile("decrypt.txt");// read from decrypt.txt
+		// System.out.println(ct);
+		// System.out.println(dt);
 
+		s.alphabetStatistics(ct);
+
+		s.frequency(ct, 0, 2);
+		s.frequency(ct, 0, 3);
+		s.frequency(ct, 0, 4);
+		s.frequency(ct, 0, 5);
+		s.frequency(ct, 0, 6);
+		s.frequency(ct, 0, 7);
+
+		String keyword = s.guessingKeyword(ct, 7);
+		String decrypttext = vc.decrypt(ct, keyword);// decrpty the ciphertext
+
+		System.out.println(keyword);
+		System.out.println(decrypttext);
+
+		op.writeFile("vigenere_nosp_decrypt.txt", decrypttext);
 	}
 
 }
--- src/breaker/Statistics.java
+++ src/breaker/Statistics.java
@@ -4,20 +4,72 @@ import java.util.Arrays;
 
 public class Statistics {
 
-	public int[] frequency(String ct,int startPosition,int guessingkeyLength){
+	public int[] frequency(String ct, int startPosition, int guessingkeyLength) {
 		int[] bucket = new int[26];
 		int index;
 		int length = ct.length();
-		for (int i = startPosition; i < length - 1; i+=guessingkeyLength) {
+		for (int i = startPosition; i < length - 1; i += guessingkeyLength) {
 			index = ct.charAt(i) - 'a';
 			bucket[index]++;
 		}
-		System.out.println("-----------guessingkeyLength: "+guessingkeyLength);
-		print(bucket,true);
+		System.out.println("GuessingkeyLength: " + guessingkeyLength);
+		System.out
+				.println("-------------------------------------------------------------");
+		print(bucket, true);
 		max(bucket);
+
+		double minP = bucket[0] / (double) length * (guessingkeyLength * 100);
+		double maxP = bucket[bucket.length - 1] / (double) length
+				* (guessingkeyLength * 100);
+		System.out.println("min:" + bucket[0]);
+		System.out.println("max:" + bucket[bucket.length - 1]);
+		System.out.println("minProb:" + minP);
+		System.out.println("maxProb:" + maxP);
+		System.out.println("maxProb-minProb: " + (maxP - minP));
+		System.out.println();
 		return bucket;
 	}
-	
+
+	public String guessingKeyword(String ct, int guessingkeyLength) {
+
+		final char highestFrequencyLetter = 'e';// magic character
+		StringBuilder sb = new StringBuilder();
+		int index;
+		int length = ct.length();
+
+		for (int k = 0; k < guessingkeyLength; k++) {
+			int[] bucket = new int[26];
+
+			for (int i = k; i < length - 1; i += guessingkeyLength) {
+				index = ct.charAt(i) - 'a';
+				bucket[index]++;
+			}
+			char maxFChar = getMaxCharacter(bucket);
+			System.out.println(maxFChar);
+			
+			int newV = (maxFChar - highestFrequencyLetter) % 26 ;
+			if(newV<0)
+				newV+=26;
+			
+			char key = (char) (newV + 97);
+			sb.append(key);
+		}
+		
+		
+		return sb.toString();
+	}
+
+	private char getMaxCharacter(int[] bucket) {
+		int maxIndex = 0;
+		for (int i = 1; i < bucket.length; i++) {
+			if (bucket[i] > bucket[maxIndex])
+				maxIndex = i;
+		}
+		char maxFChar = (char) (maxIndex + 'a');
+		return maxFChar;
+
+	}
+
 	public int[] alphabetStatistics(String ct) {
 
 		int[] bucket = new int[26];
@@ -27,7 +79,7 @@ public class Statistics {
 			index = ct.charAt(i) - 'a';
 			bucket[index]++;
 		}
-		print(bucket,true);
+		print(bucket, true);
 		max(bucket);
 		return bucket;
 	}
@@ -35,16 +87,16 @@ public class Statistics {
 	private void max(int[] bucket) {
 		Arrays.sort(bucket);
 		System.out.println("-----------left to right, increasing-----------");
-		print(bucket,false);
+		print(bucket, false);
 
 	}
 
 	private void print(int[] bucket, boolean withIndex) {
-		
+
 		for (int i = 0; i < bucket.length; i++) {
 
 			System.out.print(bucket[i]);
-			if(bucket[i]/100==0)//2 digits
+			if (bucket[i] / 100 == 0)// 2 digits
 				System.out.print("  ");
 			else
 				System.out.print(" ");
